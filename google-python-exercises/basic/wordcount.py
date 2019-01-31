cfffac2742c04071670863f4aaefde50d037459b
Abdulaziz AlFayez <zo555@windowslive.com>
2019-01-31 16:34:10 +0000

    day homework

--- google-python-exercises/basic/wordcount.py
+++ google-python-exercises/basic/wordcount.py
@@ -38,7 +38,65 @@ print_words() and print_top().
 """
 
 import sys
+def find_every(filename):
+  
+  f=open(filename,'rU') # this to open the file and read from it 
+  info={}
+  for l in f:
+    ws=l.split()
+    for w in ws:
+      w=w.lower()
+      if not w  in info:
+        info[w] = 1
+      else:
+        info[w] += 1
+  f.close()
+  return info
 
+def print_words(filename):
+  info=find_every(filename)
+  s=sorted(info.items())
+  for ss in s:
+    print ss
+  #print info
+  
+  return info
+  
+  
+def print_top(filename):
+  info=find_every(filename)
+  
+  
+  if len(info)<20:
+    ww=sorted(info.items(), key=lambda x:x[1])
+    
+    i=-1
+    j=len(ww)*(1-2)
+    
+    while i!=j:
+      
+    #xfor k in ww:
+      print( ww[i][0]+ ":"+ str(ww[i][1])) 
+     # print info[l]
+      i -= 1
+      
+  else:
+    ww=sorted(info.items(), key=lambda x:x[1])
+    i=-1
+    j=-20
+    
+    while i!=j:
+      print( ww[i][0]+ ":"+ str(ww[i][1])) 
+      i -= 1
+    
+  
+  
+  """i was lenning on the pravious funcation and i was about to take the 20 biggest numbers between them """
+  
+  
+  
+  
+  return info
 # +++your code here+++
 # Define print_words(filename) and print_top(filename) functions.
 # You could write a helper utility function that reads a file
